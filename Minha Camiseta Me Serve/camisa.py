1fce3943793f2832b02dfcadaa68635ca30ba13c
Maria <maria.clr.dias@gmail.com>
2018-10-30 18:31:08 -0200

    commit

--- Minha Camiseta Me Serve/camisa.py
+++ Minha Camiseta Me Serve/camisa.py
@@ -3,23 +3,42 @@ from networkx.algorithms import bipartite
 
 if __name__ == "__main__":
 
-    print("oi")
-    str = input("")
-    casos = int(str)
+    ler = input("")
+    casos = int(ler)
 
-    for i in casos:
-      str = input("")
-      camisetas, voluntarios = str.split(" ")
+    for i in range(0,casos):
+      ler = input("")
+      camisetas, voluntarios = ler.split(" ")
 
-      camistas = int(camisetas)
+      numCamisas = int(camisetas)
       voluntarios = int(voluntarios)
 
       B = nx.Graph()
 
-      B.add_nodes_from(['XXL','XL','L','M','S','XS'], bipartite=0)
+      camisas = ["XXL","XL","L","M","S","XS"]
 
-      for j in voluntarios:
-        B.add_nodes_from(j, bipartite=1)
-        str = input("")
-        a, b = str.split(" ")
-        B.add_edges_from([(a,j), (b,j)])
+
+      for i in camisas:
+        for j in range (0, int(numCamisas/6)):
+          B.add_node((i+str(j)), bipartite=0)
+
+      for i in range (0, voluntarios):
+        B.add_nodes_from(str(j), bipartite=1)
+         #lendo as opções
+        ler = input("")
+        a, b = ler.split(" ")
+        for j in (0, numCamisas/6):
+          B.add_edge(a+str(j),i)
+
+
+      #mostrar o grafo print(B.edges())
+
+      M = nx.maximal_matching(B)
+
+      if len(M) == voluntarios:
+        print("YES")
+      else:
+        print("NO")
+
+      # B.add_edges_from([('A',1), ('B',1), ('C',1), ('C',3), ('D',2), ('E',3),
+      # ('E',4)])
